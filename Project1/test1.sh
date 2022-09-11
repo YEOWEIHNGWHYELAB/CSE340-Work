1f27cf5faac26990113e771dc1491214572e4854
YEOWEIHNGWHYELAB <yeoweihngywh@gmail.com>
2022-09-10 23:03:30 -0700

    Project 1 Test Code

--- Project1/test1.sh
+++ Project1/test1.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-if [ ! -d "./provided_tests" ]; then
+g++ -std=c++11 parser.cc lexer.cc inputbuf.cc
+
+TEST_FOLDER="./provided_tests"
+
+SPECIAL_TEST_FOLDERS=(
+	"Syntax_Error_General" "SNYTAX ERORR"
+	"Syntax_Error_Expr" "SYNTAX ERROR IN"
+)
+
+if [ ! -d "${TEST_FOLDER}" ]; then
     echo "Error: tests directory not found!"
     exit 1
 fi
@@ -20,7 +29,7 @@ let all=0
 
 mkdir -p ./output
 
-for test_file in $(find ./provided_tests -type f -name "*.txt" | sort); do
+for test_file in $(find "${TEST_FOLDER}" -type f -name "*.txt" | sort); do
     all=$((all+1))
     name=`basename ${test_file} .txt`
     expected_file=${test_file}.expected
@@ -30,48 +39,58 @@ for test_file in $(find ./provided_tests -type f -name "*.txt" | sort); do
 
 
     folder_name="$(cut -d'/' -f3 <<<"${test_file}")"
-	
-    if [ ${folder_name} = "Syntax_Error" ]; then
-	    	diff -Bw ${expected_file} ${output_file} > ${diff_file}
-	
-		out=$(<${output_file})
-		exp=$(<${expected_file})
 
-		if [[ ${exp} == "SYNTAX ERROR" ]]; then
-			if [[ ${out} == "SYNTAX ERROR" ]]; then
-				count=$((count+1))
-				echo "${name}: OK"
+	for special_index in seq 0 2 ${#SPECIAL_TEST_FOLDERS[@]}; do
+		special_name=${SPECIAL_TEST_FOLDERS[${special_index}]}
+		special_test=${SPECIAL_TEST_FOLDERS[$((special_index+1))]}
+
+		if [ "${folder_name}" = "${special_name}" ]; then
+			diff -Bw ${expected_file} ${output_file} > ${diff_file}
+
+			out=$(<${output_file})
+			exp=$(<${expected_file})
+
+			if [[ ${exp} == *${special_test}* ]]; then
+				if [[ -s ${diff_file} ]]; then
+					echo "${name}: Output does not match expected:"
+					echo "--------------------------------------------------------"
+					cat ${diff_file}
+				else
+					count=$((count+1))
+					echo "${name}: OK"
+				fi
 			else
-				echo "${name}: Output does not match expected:"
-				echo "--------------------------------------------------------"
-				cat ${diff_file}
+				if [[ ${out} == *${special_test}* ]]; then
+					echo "${name}: Output does not match expected:"
+					echo "--------------------------------------------------------"
+					cat ${diff_file}
+				else
+					count=$((count+1))
+					echo "${name}: OK"
+				fi
 			fi
-		else
-			if [[ ${out} == "SYNTAX ERROR" ]]; then
-				echo "${name}: Output does not match expected:"
-				echo "--------------------------------------------------------"
-				cat ${diff_file}
-			else
-				count=$((count+1))
-				echo "${name}: OK"
-			fi
+
+			echo "========================================================"
+			rm -f ${output_file}
+			rm -f ${diff_file}
+
+			continue 2
 		fi
-            
-    else
+	done
 
-
-	    diff -Bw ${expected_file} ${output_file} > ${diff_file}
-		
-	    echo
-	    if [ -s ${diff_file} ]; then
+	# General folder handling
+	
+	diff -Bw ${expected_file} ${output_file} > ${diff_file}
+	
+	echo
+	if [ -s ${diff_file} ]; then
 		echo "${name}: Output does not match expected:"
 		echo "--------------------------------------------------------"
 		cat ${diff_file}
-	    else
+	else
 		count=$((count+1))
 		echo "${name}: OK"
-	    fi
-    fi
+	fi
     echo "========================================================"
     rm -f ${output_file}
     rm -f ${diff_file}
